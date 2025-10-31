96df202048c0f737ff7849bee7d92ecb4461c408
ZiadMedhat33 <ziadmedhat301@gmail.com>
2025-10-31 13:57:26 +0200

    a

--- app/src/Components/Header/Header.js
+++ app/src/Components/Header/Header.js
@@ -1,31 +1,43 @@
 import { Link } from 'react-router-dom';
-import './header-style.css'; 
-export default function Header(){
-    return(
-        <>
-            <nav>
-                <div className="nav-upper-part">
+import React from "react";
+
+import './header-style.css';
+export default function Header() {
+    const [menuOpen, setMenuOpen] = React.useState(false)
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen)
+        console.log(menuOpen)
+    }
+    const [loggedIn, setLoggedIn] = React.useState(false)
+    function login() {
+        setLoggedIn(!loggedIn)
+    }
+    return (
+        <nav>
+            <div className="nav-upper-part">
                 <div className="nav-title">
+                    <div className="burger-icon" onClick={toggleMenu}><i class="fa-solid fa-bars"></i></div>
                     EventPlanner
                 </div>
-                <div className="nav-buttons">
-                    <Link to={"/login"}><button className="login-button">login</button></Link>
-                    <Link to={"/signup"}><button className="signup-button">signup</button></Link>
-                </div>
-                <div className="burger-icon"><i class="fa-solid fa-bars"></i></div>
-                </div>
-                <div className="nav-lower-part">
-                    <div className="nav-items-contaienr">
-                        <p className="nav-item">placeholder</p>
-                        <p className="nav-item">placeholder</p>
-                        <p className="nav-item">placeholder</p>
-                        <p className="nav-item">placeholder</p>
-                        <p className="nav-item">placeholder</p>
-                        <p className="nav-item">placeholder</p>
-                        <p className="nav-item">placeholder</p>
+                {!loggedIn ?
+                    <div className="nav-buttons">
+                        <Link to={"/login"}><button className="login-button" onClick={login}>login</button></Link>
+                        <Link to={"/signup"}><button className="signup-button">signup</button></Link>
                     </div>
+                    :
+                    <div className="nav-buttons">
+                        <Link to={"/login"}><button className="login-button" onClick={login}>logout</button></Link>
+                    </div>
+                }
+
+            </div>
+            <div className={`nav-lower-part ${menuOpen ? "open" : ""}`}>
+                <div className="nav-items-contaienr">
+                    <p className="nav-item">events</p>
+                    <p className="nav-item">profile</p>
                 </div>
-            </nav>
-        </>
+            </div>
+        </nav>
     )
 }
+
